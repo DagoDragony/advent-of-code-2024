61094d7f6200bef6ad3f910b8db479255d1b6007
DagoDragony <dago.dragony@gmail.com>
2024-12-03 22:00:15 +0200

    auto update

--- day3_1.py
+++ day3_1.py
@@ -6,7 +6,7 @@ from itertools import tee, islice
 # Get the absolute path of the current script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
 # file_path = os.path.join(script_dir, 'inputs/input_3_1_example.txt')
-file_path = os.path.join(script_dir, 'inputs/input_3_1.txt')
+file_path = os.path.join(script_dir, 'inputs/input_3_1_example.txt')
 
 print(f"Reading file {file_path}")
 print(f"PROCESSING PART 1...")
@@ -21,18 +21,15 @@ def solve1(content):
     # print(raw_commands)
     return sum([int(a) * int(b) for a, b in raw_commands])
 
-def isdont(it):
-    for c in "don't()":
-        if(it)
-
-
-def solve1_v2(content):
-    it = iter(content)
-    iter1, iter2 = tee(it, 2)
-    # even_chars = islice(iter1, 5)
-    isdont(tee(content))
-
+def solve2(content):
+    start_pattern = r"^(.*?)(don't()|do())"
+    do_patern = r"do\(\).*?(don't()|$)"
+    start = re.findall(start_pattern, content)
+    dos = re.findall(start_pattern, content)
+    print(f"Start {start}")
+    print(f"Do's {dos}")
 
 
 
 print(f"result1: {solve1(content)}")
+print(f"result2: {solve2(content)}")
