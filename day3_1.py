a71d9800f31927d8568e135a0749b010d8f03c91
DagoDragony <dago.dragony@gmail.com>
2024-12-03 08:50:51 +0200

    auto update

--- day3_1.py
+++ day3_1.py
@@ -1,54 +1,38 @@
 import os
+import re
+
+from itertools import tee, islice
 
 # Get the absolute path of the current script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, 'inputs/input_2_1.txt')
-# file_path = os.path.join(script_dir, 'inputs/input_2_1_example.txt')
+# file_path = os.path.join(script_dir, 'inputs/input_3_1_example.txt')
+file_path = os.path.join(script_dir, 'inputs/input_3_1.txt')
 
 print(f"Reading file {file_path}")
-print(f"PROCESSING PART 2...")
-level_list = []
+print(f"PROCESSING PART 1...")
+content = ""
 with open(file_path, 'r') as file:
-    lines = file.readlines()
-    for line in lines:
-        level_list.append(list(map(int, line.split(' '))))
+    content = file.read()
+# print(content)
 
-def check_levels(levels):
-    print(f"checking {levels}")
-    ascending = True if(levels[0] - levels[1] < 0) else False
-    for i in range(len(levels) - 1):
-        diff = levels[i] - levels[i+1]
-        # print(diff)
-        if abs(diff) < 1 or abs(diff) > 3:
-            return False
-        if ascending and diff > 0:
-            return False
-        if not ascending and diff < 0:
-            return False
-    return True
+def solve1(content):
+    pattern = r"mul\((\d+),(\d+)\)"
+    raw_commands = re.findall(pattern, content)
+    # print(raw_commands)
+    return sum([int(a) * int(b) for a, b in raw_commands])
+
+def isdont(it):
+    for c in "don't()":
+        if(it)
 
 
-print(level_list)
-safe_count = 0
-for levels in level_list:
-    if(len(levels) > 1):
-        ascending = True if(levels[0] - levels[1] < 0) else False
-    second_chance = False
-    succeeded = True
-    succeeded = check_levels(levels)
-    if not succeeded:
-        for i in range(len(levels)):
-            print(f"i={i}")
-            new_levels = list(levels)
-            print(f"removing {new_levels[i]}")
-            del new_levels[i]
-            good = check_levels(new_levels)
-            if good:
-                print("succeeded")
-                succeeded = True
-                break
-    print(succeeded)
-    if succeeded:
-        safe_count += 1
+def solve1_v2(content):
+    it = iter(content)
+    iter1, iter2 = tee(it, 2)
+    # even_chars = islice(iter1, 5)
+    isdont(tee(content))
 
-print(f"result2: {safe_count}")
+
+
+
+print(f"result1: {solve1(content)}")
