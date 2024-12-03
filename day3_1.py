d554e7cb38028b07beeba598565c83939c53286f
DagoDragony <dago.dragony@gmail.com>
2024-12-03 23:00:15 +0200

    auto update

--- day3_1.py
+++ day3_1.py
@@ -6,30 +6,30 @@ from itertools import tee, islice
 # Get the absolute path of the current script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
 # file_path = os.path.join(script_dir, 'inputs/input_3_1_example.txt')
-file_path = os.path.join(script_dir, 'inputs/input_3_1_example.txt')
+# file_path = os.path.join(script_dir, 'inputs/input_3_2_example.txt')
+file_path = os.path.join(script_dir, 'inputs/input_3_1.txt')
 
 print(f"Reading file {file_path}")
 print(f"PROCESSING PART 1...")
 content = ""
 with open(file_path, 'r') as file:
     content = file.read()
-# print(content)
+print(content)
 
-def solve1(content):
-    pattern = r"mul\((\d+),(\d+)\)"
-    raw_commands = re.findall(pattern, content)
+mul_pattern = r"mul\((\d+),(\d+)\)"
+
+def get_mul_result(report):
+    raw_commands = re.findall(mul_pattern, report)
     # print(raw_commands)
     return sum([int(a) * int(b) for a, b in raw_commands])
 
-def solve2(content):
-    start_pattern = r"^(.*?)(don't()|do())"
-    do_patern = r"do\(\).*?(don't()|$)"
-    start = re.findall(start_pattern, content)
-    dos = re.findall(start_pattern, content)
-    print(f"Start {start}")
-    print(f"Do's {dos}")
+def solve1(report):
+    return get_mul_result(report)
 
-
+def solve2(report):
+    unfiltered_dos = report.split("do()")
+    filtered_dos = "".join([unfiltered_do.split("don't")[0] for unfiltered_do in unfiltered_dos])
+    return get_mul_result(filtered_dos)
 
 print(f"result1: {solve1(content)}")
 print(f"result2: {solve2(content)}")
