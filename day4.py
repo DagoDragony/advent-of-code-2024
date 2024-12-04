5f18e900929b184a0efb41803653954a9c2fdae3
daliusg <daliusg@wix.com>
2024-12-04 17:00:15 +0200

    auto update

--- day4.py
+++ day4.py
@@ -1,9 +1,13 @@
 import os
 
+import numpy as np
+
 # Get the absolute path of the current script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
 # file_path = os.path.join(script_dir, 'inputs/input_d4_example1_custom.txt')
-file_path = os.path.join(script_dir, 'inputs/input_d4_example1.txt')
+# file_path = os.path.join(script_dir, 'inputs/input_d4_example1_custom2.txt')
+# file_path = os.path.join(script_dir, 'inputs/input_d4_example1.txt')
+file_path = os.path.join(script_dir, 'inputs/input_d4.txt')
 
 print(f"Reading file {file_path}")
 print("PROCESSING PART 1...")
@@ -55,8 +59,76 @@ def check_XMAS(i, j, puzzle):
 # count = sum([check_XMAS(i, j, puzzle_map) for i in range(len(puzzle_map)) for j in range(len(puzzle_map[0]))])
 # print(f"count: {count}")
 
-count = check_XMAS(0, 4, puzzle_map)
+# right = (0, 1)
+# left = (0, -1)
+# up = (-1, 0)
+# down = (1, 0)
+# ne = (-1, 1)
+# nw = (-1, -1)
+# se = (1, 1)
+# sw = (1, -1)
+xmas_letters = "XMAS"
+all_directions = [
+    (0, 1),
+    (0, -1),
+    (1, 0),
+    (1, 1),
+    (1, -1),
+    (-1, 0),
+    (-1, 1),
+    (-1, -1)
+]
+
+def check_in_direction(i, j, direction, puzzle):
+    max_i = len(puzzle)
+    max_j = len(puzzle[0])
+    def valid_coord(_i, _j):
+        return _i >= 0 and _i < max_i and _j >= 0 and _j < max_j
+
+    print(f"Direction {direction}")
+    i_shift, j_shift = direction
+    for index in range(1, 4):
+        ni, nj = (i + index * i_shift, j + index * j_shift )
+        if not valid_coord(ni, nj):
+            return False
+        print((ni, nj), puzzle[ni][nj])
+        if puzzle[ni][nj] != xmas_letters[index]:
+            return False
+    print("Found")
+    return True
+        
+def check_XMAS(i, j, puzzle):
+    print(xmas_letters[0])
+    if puzzle[i][j] == xmas_letters[0]:
+        return sum([check_in_direction(i, j, direction, puzzle)  for direction in all_directions])
+    return 0
+
+count = sum(check_XMAS(i, j, puzzle_map) for i in range(len(puzzle_map)) for j in range(len(puzzle_map[0])))
 print(f"count: {count}")
+    
+    
+
+
+# def get_coords_to_check2(i, j, puzzle_map):
+#     max_i = len(puzzle_map)
+#     max_j = len(puzzle_map[0])
+#     def valid_coord(_i, _j):
+#         return _i >= 0 and _i < max_i and _j >= 0 and _j < max_j
+
+#     horizonal_right = [(i, j + s) for s in range(1, 4) if valid_coord(i, j+s)]
+#     horizonal_left = [(i, j - s) for s in range(1, 4) if valid_coord(i, j -s)]
+
+
+
+#     coords = [(oi + i, oj + j) for i in [-1, 0, 1] for j in [-1, 0, 1] if not (i == 0 and j == i)]
+#     result = [(ci, cj) for ci, cj in coords if ci >= 0 and cj >= 0 and ci < max_i and cj < max_j]
+#     # print(result)
+#     return result
+
+# count = check_XMAS(0, 4, puzzle_map)
+# print(f"count: {count}")
+
+# print(np.add((1, 2), (3, 4)))
 
 # def find_words(i, puzzle_input):
 #     if puzzle_input[i] == 'X':
