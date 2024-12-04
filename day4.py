b446c4158262e6bd01fa0fba8857d70491206e6f
DagoDragony <dago.dragony@gmail.com>
2024-12-04 08:52:21 +0200

    auto update

--- day4.py
+++ day4.py
@@ -10,26 +10,32 @@ file_path = os.path.join(script_dir, 'inputs/input_d4_example1.txt')
 
 print(f"Reading file {file_path}")
 print(f"PROCESSING PART 1...")
-content = ""
+puzzle_map = []
 with open(file_path, 'r') as file:
-    content = file.read()
-print(content)
+    lines = file.readlines()
+    for line in lines:
+        puzzle_map.append(line.strip())
 
-@dataclass
-class Coord:
-    i: int
-    j: int
+print(puzzle_map)
 
-def get_coords_to_check
-
-def find_words(i, puzzle_input):
-    if puzzle_input[i] == 'X':
-
+def get_coords_to_check(puzzle_map, coord):
+    max_i = len(puzzle_map[0])
+    max_j = len(puzzle_map)
+    coords = [(coord[0] + i, coord[1] + j) for i in [-1, 0, 1] for j in [-1, 0, 1] if not(i == 0 and j == i)]
+    return [ c for c in coords if c[0] < max_i and c[1] < max_j]
 
 
-def solve1(puzzle_input):
+get_coords_to_check(puzzle_map)
+
+
+# def find_words(i, puzzle_input):
+#     if puzzle_input[i] == 'X':
 
 
 
-print(f"result1: {solve1(content)}")
-print(f"result2: {solve2(content)}")
+# def solve1(puzzle_input):
+
+
+
+# print(f"result1: {solve1(content)}")
+# print(f"result2: {solve2(content)}")
