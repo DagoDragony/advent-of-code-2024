6a877a0ca552b5674307fedf73a4ddedaac6041b
DagoDragony <dago.dragony@gmail.com>
2024-12-06 23:00:15 +0200

    auto update

--- day6_v2.py
+++ day6_v2.py
@@ -2,8 +2,8 @@ import os
 
 # Get the absolute path of the current script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, 'inputs/input_d6_example1.txt')
-# file_path = os.path.join(script_dir, 'inputs/input_d6.txt')
+# file_path = os.path.join(script_dir, 'inputs/input_d6_example1.txt')
+file_path = os.path.join(script_dir, 'inputs/input_d6.txt')
 
 print(f"Reading file {file_path}")
 print("PROCESSING PART 1...")
@@ -45,22 +45,23 @@ def process_path_history(c, direction, path_history):
         else:
             path_history[c].add(direction)
     else:
-        path_history[c] = set(direction)
+        path_history[c] = set([direction])
     return False
 
 def is_loop_walk(i, j, map):
     path_history = {}
     direction = (-1, 0)
-    path_history[(i, j)] = set(direction)
+    path_history[(i, j)] = set([direction])
     while(True):
         ni, nj = (i + direction[0], j + direction[1])
         if outside_map(ni, nj, map):
             # print(ni, nj)
-            break;
+            break
         elif map[ni][nj] == '#' or map[ni][nj] == 'O':
             # print("#", ni, nj)
             direction = righ_changes[direction]
             # print(direction)
+            process_path_history((i, j), direction, path_history)
             i, j = (i + direction[0], j + direction[1]) 
 
             if process_path_history((i, j), direction, path_history):
@@ -73,13 +74,14 @@ def is_loop_walk(i, j, map):
                 return path_history
     return {}
 
+def move(i, j, direction):
+    return (i + direction[0], j + direction[1])
 
 def get_initial_path(i, j, map):
     direction = (-1, 0)
-    passed_path = set()
-    passed_path.add((i, j))
+    passed_path = set([(i, j)])
     while(True):
-        ni, nj = (i + direction[0], j + direction[1])
+        ni, nj = move(i, j, direction)
         if outside_map(ni, nj, map):
             # print(ni, nj)
             break;
@@ -87,15 +89,13 @@ def get_initial_path(i, j, map):
             # print("#", ni, nj)
             direction = righ_changes[direction]
             # print(direction)
-            i, j = (i + direction[0], j + direction[1]) 
-            if(map[i][j] != 'X'):
-                passed_path.add((i, j))
+            i, j = move(i, j, direction)
+            passed_path.add((i, j))
             # print(i, j)
         else:
             i, j = (ni, nj)
             # print(i, j)
-            if(map[i][j] != 'X'):
-                passed_path.add((i, j))
+            passed_path.add((i, j))
         # print(already_walked)
 
     return passed_path
@@ -103,9 +103,14 @@ def get_initial_path(i, j, map):
 start_i, start_j = get_current_position(lab_map)
 current_path = get_initial_path(start_i, start_j, lab_map)
 print("-"*100)    
-print(len(current_path))
+print(f"Result1: {len(current_path)}")
 
+already_added = set()
 def add_obstacle(i, j, map):
+    if (i, j) in already_added:
+        raise Exception()
+    already_added.add((i, j))
+
     # print(map)
     new_map = list(map)
     new_line = list(map[i])
@@ -114,32 +119,46 @@ def add_obstacle(i, j, map):
     return new_map
 
 def get_map_combinations(si, sj, map, path):
-    return [add_obstacle(i, j, map) for i, j in path if i != si and j != sj]
+    return [add_obstacle(i, j, map) for i, j in path if not (i == si and j == sj)]
 
 def print_infinite_path(path, m):
     for i in range(len(m)):
         line = list(m[i])
         for j in range(len(line)):
             if (i, j) in path:
-                line[j] = "X"
+                h = list(path[(i, j)])
+                # print("h")
+                # print(h)
+                im = 0
+                jm = 0
+                for c in h:
+                    hi, hj = c
+                    im += abs(hi)
+                    jm += abs(hj)
+                if im != 0 and jm != 0:
+                    line[j] = "+"
+                elif im != 0:
+                    line[j] = "|"
+                elif jm != 0:
+                    line[j] = "-"
         print("".join(line))
     print()
 
+# for c in sorted(current_path):
+#     print(c)
+
+# print(start_i, start_j)
+
 inifinite_loops = 0
 for m in get_map_combinations(start_i, start_j, lab_map, current_path):
+    # print("Combination")
+    # print_map(m)
     infinite_path = is_loop_walk(start_i, start_j, m)
-    if len(infinite_path):
+    if len(infinite_path) > 0:
         inifinite_loops += 1
-        print_infinite_path(infinite_path, m)
+        # print_infinite_path(infinite_path, m)
 
-# inifinite_loops = sum([is_loop_walk(start_i, start_j, m) for m in get_map_combinations(start_i, start_j, lab_map, current_path)])
-print(inifinite_loops)
-
-
-# possible_location_of_obstacle = [(i, j) for i in range(len(lab_map)) for j in range(len(lab_map[0])) if lab_map[i][j] == "."]
-
-
-
+print(f"Result2: {inifinite_loops}")
 
 # print_map(lab_map)
 # # print(f"result2: {solve2(content)}")
