37f2ff693cf96bc3c12f7fc7b434acd2458a01a5
daliusg <daliusg@wix.com>
2024-12-07 09:00:12 +0200

    auto update

--- day7.py
+++ day7.py
@@ -1,9 +1,11 @@
 import os
+from itertools import permutations
+import sys
 
 # Get the absolute path of the current script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, 'inputs/input_d7_example1.txt')
-# file_path = os.path.join(script_dir, 'inputs/input_d7.txt')
+# file_path = os.path.join(script_dir, 'inputs/input_d7_example1.txt')
+file_path = os.path.join(script_dir, 'inputs/input_d7.txt')
 
 print(f"Reading file {file_path}")
 print("PROCESSING PART 1...")
@@ -17,5 +19,48 @@ with open(file_path, 'r') as file:
 for equation in equations:
     print(equation)
 
+def calc_answer(operators, numbers, max_answer):
+    agg_answer = numbers[0]
+    for i in range(len(operators )):
+        if agg_answer > max_answer:
+            return sys.maxsize
+        if agg_answer > answer:
+            break
+        if operators[i] == "+":
+            agg_answer += numbers[i + 1]
+        elif operators[i] == "*": 
+            agg_answer *= numbers[i + 1]
+    
+    print(str(numbers[0]) + " " + "".join([f"{operators[i]} {numbers[i + 1]} " for i in range(len(operators))]) + f" = {agg_answer}")
+    return agg_answer
+
+
+def is_equation_possible(answer, numbers):
+    # print(f"{answer}: {numbers}")
+    operators = ["+" for _ in range(len(numbers) - 1)]
+    if calc_answer(operators, numbers, answer) == answer:
+        return True
+    for i in range(len(operators)):
+        operators[i] = "*"
+        operators_sets = set(permutations(operators))
+        print(operators_sets)
+        for operators_set in operators_sets:
+            if calc_answer(operators_set, numbers, answer) == answer:
+                correct_equation = str(numbers[0]) + " " + "".join([f"{operators[i]} {numbers[i + 1]} " for i in range(len(operators))])
+                return correct_equation
+    return None
+        
+            
 print("-"*100)    
+possible_count = 0
+for answer, numbers in equations:
+    result = is_equation_possible(answer, numbers)
+    if result != None:
+        possible_count += 1
+        print(result)
+
+print("-"*100)    
+print(f"Result: {possible_count}")
+    
+
 # # print(f"result1: {solve1(content)}")
