3a9ee76e82d17b384d8a409e3081b019b43239a1
daliusg <daliusg@wix.com>
2024-12-08 12:00:13 +0200

    auto update

--- day8.py
+++ day8.py
@@ -6,6 +6,8 @@ import itertools
 import sys
 import re
 
+itertools.combinations
+
 # Get the absolute path of the current script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_dir, 'inputs/input_d8_example1.txt')
@@ -36,8 +38,38 @@ def find_antennas(map):
     return antennas
 
 
+def outside_map(i, j, max_i, max_j):
+    return i < 0 or j < 0 or i >= max_i or j >= max_j
+
+
+def get_combination_antinodes(c1, c2):
+    i1, j1 = c1
+    i2, j2 = c2
+    di = i1 - i2
+    dj = j1 - j2
+    return [(i1 + di, j1 + dj), (i2 - di, j2 - dj)]
+
+
+def get_same_frequency_antinodes(antennas):
+    print(antennas)
+    for antenna_name, coords  in antennas:
+        combinations = itertools.combinations(coords, r=2)
+        for c1, c2 in combinations:
+            get_combination_antinodes(c1, c2)
+
+def solve1(map):
+    antinodes = []
+    max_i = len(map)
+    max_j = len(map[0])
+    for antennas in find_antennas(map):
+        antinodes.extend(get_same_frequency_antinodes(antennas))
+
+    return [coord for coord in antinodes if not outside_map(coord, max_i, max_j)]
+
             
 find_antennas(antenas_map) 
+
+print(solve1(antenas_map))
 
 print("-"*100)    
 print(f"Result: ")
