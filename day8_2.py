e14023fa108cdde637370cae3932e62995758409
daliusg <daliusg@wix.com>
2024-12-08 13:00:13 +0200

    auto update

--- day8_2.py
+++ day8_2.py
@@ -1,14 +1,6 @@
 import os
-from typing import List, AnyStr
-# from itertools import distinct_permutations
-from more_itertools import distinct_permutations
 import itertools
-import sys
-import re
 
-itertools.combinations
-
-# Get the absolute path of the current script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
 # file_path = os.path.join(script_dir, 'inputs/input_d8_example1.txt')
 file_path = os.path.join(script_dir, 'inputs/input_d8.txt')
@@ -18,11 +10,12 @@ print("PROCESSING PART 1...")
 antenas_map = []
 with open(file_path, 'r') as file:
     for line in file.readlines():
-        # print(line)
         antenas_map.append(line.strip())
+
 
 for line in antenas_map:
     print(line)
+
 
 def find_antennas(map):
     antennas = {}
@@ -34,7 +27,7 @@ def find_antennas(map):
                     antennas[symbol].append((i, j))
                 else:
                     antennas[symbol] = [(i, j)]
-    print(antennas)
+    # print(antennas)
     return antennas
 
 
@@ -42,21 +35,35 @@ def outside_map(i, j, max_i, max_j):
     return i < 0 or j < 0 or i >= max_i or j >= max_j
 
 
-def get_combination_antinodes(c1, c2):
+def get_combination_antinodes(c1, c2, max_i, max_j):
     i1, j1 = c1
     i2, j2 = c2
     di = i1 - i2
     dj = j1 - j2
-    return [(i1 + di, j1 + dj), (i2 - di, j2 - dj)]
+
+    def get_side_antinodes(i, j, di, dj):
+        antinodes = []
+        while not outside_map(i, j, max_i, max_j):
+            antinodes.append((i, j))
+            i, j = (i + di, j + dj)
+
+        return antinodes
+    
+
+    final_antinodes = [] 
+    final_antinodes.extend(get_side_antinodes(*c1, di, dj))
+    final_antinodes.extend(get_side_antinodes(*c2, -di, -dj))
+
+    return final_antinodes
 
 
-def get_same_frequency_antinodes(antennas):
+def get_same_frequency_antinodes(antennas, max_i, max_j):
     # print(f"antennas {antennas}")
     # print(type(antennas))
     antenna_name, coords = antennas
     combinations = itertools.combinations(coords, r=2)
     antinodes_list = []
-    [antinodes_list.extend(get_combination_antinodes(c1, c2)) for c1, c2 in combinations]
+    [antinodes_list.extend(get_combination_antinodes(c1, c2, max_i, max_j)) for c1, c2 in combinations]
     return antinodes_list
         
 
@@ -65,18 +72,11 @@ def solve1(map):
     max_i = len(map)
     max_j = len(map[0])
     for antennas in find_antennas(map).items():
-        print(f"0antennas {antennas}")
-        antinodes.extend(get_same_frequency_antinodes(antennas))
-
-    return [coord for coord in antinodes if not outside_map(*coord, max_i, max_j)]
-
-            
-find_antennas(antenas_map) 
-
-print(len(set(solve1(antenas_map))))
+        # print(f"0antennas {antennas}")
+        antinodes.extend(get_same_frequency_antinodes(antennas, max_i, max_j))
+    return antinodes
 
 print("-"*100)    
-print(f"Result: ")
+print(f"Result2: {len(set(solve1(antenas_map)))}")
     
-
 # # print(f"result1: {solve1(content)}")
