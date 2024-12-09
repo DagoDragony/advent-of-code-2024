2397526fbd021f46f718e15c987d43ce9ca8ec23
daliusg <daliusg@wix.com>
2024-12-09 10:00:13 +0200

    auto update

--- day9.py
+++ day9.py
@@ -6,8 +6,8 @@ itertools.combinations
 
 # Get the absolute path of the current script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
-# file_path = os.path.join(script_dir, 'inputs/input_d8_example1.txt')
-file_path = os.path.join(script_dir, 'inputs/input_d8.txt')
+file_path = os.path.join(script_dir, 'inputs/input_d9_example1.txt')
+# file_path = os.path.join(script_dir, 'inputs/input_d9.txt')
 
 print(f"Reading file {file_path}")
 print("PROCESSING PART 1...")
@@ -15,41 +15,49 @@ with open(file_path, 'r') as file:
         disk_map = file.read()
 
 print(disk_map)
-def find_antennas(map):
-    antennas = defaultdict(list)
-    for i, row in enumerate(map):
-        for j, symbol in enumerate(row):
-            if symbol.isalnum():
-                antennas[symbol].append((i, j))
-    return antennas
 
+def expand_disk_map(map):
+    expanded_disk_map = []
+    for i, symbol in enumerate(map):
+        nmb = int(symbol)
+        if i % 2 == 0:
+            expanded_disk_map.append([str(i // 2) for n in range(nmb)])
+        else:
+            expanded_disk_map.append(["." for n in range(nmb)])
+    return expanded_disk_map
 
-def outside_map(i, j, max_i, max_j):
-    return i < 0 or j < 0 or i >= max_i or j >= max_j
+def compact_disk_map(expaded_disk_map):
+    dm = list(expanded_disk_map)
+    is_full = False
+    i = 0
+    empty_spaces = [(i, j) for i, block in enumerate(expanded_disk_map) for j in range(len(block)) if i % 2 == 1]
+    numbers = list(reversed([(i, j) for i, block in enumerate(expanded_disk_map) for j in range(len(block)) if i % 2 == 0]))
 
+    last_emptied = (9999999, 99999999)
+    for i in range(len(empty_spaces)):
+        ei, ej = empty_spaces[i]
+        ni, nj = numbers[i]
 
-def get_combination_antinodes(c1, c2):
-    i1, j1 = c1
-    i2, j2 = c2
-    di, dj = i1 - i2, j1 - j2
-    return [(i1 + di, j1 + dj), (i2 - di, j2 - dj)]
+        if expaded_disk_map[ni][nj] == '.':
+            print(ei, ej, ni, nj)
+            break
 
+        if last_emptied < (ei, ej):
+            lei, lej = last_emptied
+            expanded_disk_map[lei][lej] = expaded_disk_map[ni][nj]
+        else:
+            expanded_disk_map[ei][ej] = expaded_disk_map[ni][nj]
 
-def get_same_frequency_antinodes(antennas):
-    _, coords = antennas
-    combinations = itertools.combinations(coords, r=2)
-    antinodes_list = []
-    [antinodes_list.extend(get_combination_antinodes(c1, c2)) for c1, c2 in combinations]
-    return antinodes_list
-        
+        expanded_disk_map[ni][nj] = '.'
+        last_emptied = (ni, nj)
 
-def solve1(map):
-    antinodes = []
-    max_i, max_j = len(map), len(map[0])
-    for antennas in find_antennas(map).items():
-        antinodes.extend(get_same_frequency_antinodes(antennas))
-
-    return [coord for coord in antinodes if not outside_map(*coord, max_i, max_j)]
+# 00 998 111 888 2 777 333 6 44 6 555 566
+expanded_disk_map = expand_disk_map(disk_map)
+print(expanded_disk_map)
+# print("".join(expanded_disk_map))
+compact_disk_map(expanded_disk_map)
+for l in expanded_disk_map:
+    print(l)
             
 print("-"*100)    
-print(f"Result1: {len(set(solve1(antenas_map)))}")
+print(f"Result1: ")
