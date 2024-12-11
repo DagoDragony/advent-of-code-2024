20310daa1ae7d760c7208e0c18d1c0ff0a89fb43
DagoDragony <dago.dragony@gmail.com>
2024-12-11 21:00:15 +0200

    auto update

--- day11.py
+++ day11.py
@@ -35,61 +35,105 @@ def get_stones_after_n_blinks(i_stones, blinks):
                 stones[index] = stones[index] * 2024
     return stones
 
-cache = {}
+stone_count_cache = {}
 
-def get_next_stones(stone, max_depth):
-    if stone in cache:
-        return cache[stone]
-
-
-
-    index = i + shift
-    n = stones[index]
-    if n == 0:
-        stones[index] = 1
-    elif (len(str(n)) % 2 == 0):
-        str_stone = str(n)
+def get_next_stones_2(stone):
+    if stone == 0:
+        return [1]
+    elif (len(str(stone)) % 2 == 0):
+        str_stone = str(stone)
         middle_index = int(len(str_stone) / 2)
-        stones[index] = int(str_stone[:middle_index])
-        stones[index] = int(str_stone[:middle_index])
-        stones.insert(index, int(str_stone[middle_index:]))
-        shift += 1
+        stones = [int(str_stone[:middle_index]), int(str_stone[middle_index:])]
+        return stones
     else:
-        stones[index] = stones[index] * 2024
+        stones = [stone * 2024]
+        return stones
+
+def get_stones_count(stone, left_iterations):
+    key = (stone, left_iterations)
+    if key in stone_count_cache:
+        return stone_count_cache[key]
+    if left_iterations == 1:
+        result = len(get_next_stones_2(stone))
+        stone_count_cache[key] = result
+        return result
+    else:
+        result = sum([get_stones_count(new_stone, left_iterations - 1) for new_stone in get_next_stones_2(stone)])
+        stone_count_cache[key] = result
+        return result
+
+    
+# def get_stones
+
+# def get_next_stones(stone, max_depth):
+#     if stone in cache:
+#         return cache[stone]
 
 
-def get_stones_after_n_blinks_efficient(i_stones, blinks):
-    cache = defaultdict(list)
-    stones = deque(i_stones)
-    for iteration in range(blinks):
-        get_next_stones()
-        print(iteration)
-    return stones
+#     stones = get_next_stones()
+
+
+#     if n == 0:
+#         stones[index] = 1
+#     elif (len(str(n)) % 2 == 0):
+#         str_stone = str(n)
+#         middle_index = int(len(str_stone) / 2)
+#         stones[index] = int(str_stone[:middle_index])
+#         stones[index] = int(str_stone[:middle_index])
+#         stones.insert(index, int(str_stone[middle_index:]))
+#         shift += 1
+#     else:
+#         stones[index] = stones[index] * 2024
+
+
+# def get_stones_after_n_blinks_efficient(i_stones, blinks):
+#     cache = defaultdict(list)
+#     stones = deque(i_stones)
+#     for iteration in range(blinks):
+#         get_next_stones()
+#         print(iteration)
+#     return stones
 
 print("Initial stones")
 print(initial_stones)
 print("Stones after 25 blinks")
-stones_after_25_blinks = get_stones_after_n_blinks(initial_stones, 25)
-# print(stones_after_25_blinks)
-print(len(stones_after_25_blinks))
-stones_after_75_blinks = get_stones_after_n_blinks(initial_stones, 75)
-# print(stones_after_25_blinks)
-print(len(stones_after_75_blinks))
+result_25 = sum(get_stones_count(stone, 25) for stone in initial_stones)
+print(result_25)
+print("Stones after 75 blinks")
+result_75 = sum(get_stones_count(stone, 75) for stone in initial_stones)
+print(result_75)
 
-n - {
-     iter1: n1
-     iter2: n2
-     iter3: n3
-     iter4: n4
-}
+# count = 0
+# for stone in initial_stones:
+#     print(stone)
+#     count += get_stones_count(stone, 75)
+# print(count)
 
-0 -> 1
-1 -> [2024]
-2024 -> [20, 24]
-20 -> [2, 0]
-24 -> [2, 4]
-2 -> [2*2024]
-4 -> [4*2024]
+
+
+
+
+# stones_after_25_blinks = get_stones_after_n_blinks(initial_stones, 25)
+# # print(stones_after_25_blinks)
+# print(len(stones_after_25_blinks))
+# stones_after_75_blinks = get_stones_after_n_blinks(initial_stones, 75)
+# # print(stones_after_25_blinks)
+# print(len(stones_after_75_blinks))
+
+# n - {
+#      iter1: n1
+#      iter2: n2
+#      iter3: n3
+#      iter4: n4
+# }
+
+# 0 -> 1
+# 1 -> [2024]
+# 2024 -> [20, 24]
+# 20 -> [2, 0]
+# 24 -> [2, 4]
+# 2 -> [2*2024]
+# 4 -> [4*2024]
 
 
 
