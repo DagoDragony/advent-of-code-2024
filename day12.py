91cf1939a26312678bcd59650bf156d260a56d81
daliusg <daliusg@wix.com>
2024-12-12 08:00:12 +0200

    auto update

--- day12.py
+++ day12.py
@@ -6,83 +6,15 @@ itertools.combinations
 
 # Get the absolute path of the current script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, 'inputs/input_d11_example1.txt')
-# file_path = os.path.join(script_dir, 'inputs/input_d11.txt')
+file_path = os.path.join(script_dir, 'inputs/input_d12_example1.txt')
+# file_path = os.path.join(script_dir, 'inputs/input_d12.txt')
 
 print(f"Reading file {file_path}")
 print("PROCESSING PART 1...")
 with open(file_path, 'r') as file:
-        initial_stones = [int(str_stone) for str_stone in file.read().strip().split(" ") ] 
+    garden = [line.strip() for line in file] 
 
-def get_stones_after_n_blinks(i_stones, blinks):
-    stones = deque(i_stones)
-    for iteration in range(blinks):
-        print(iteration)
-        shift = 0
-        for i in range(len(stones)):
-            index = i + shift
-            n = stones[index]
-            if n == 0:
-                stones[index] = 1
-            elif (len(str(n)) % 2 == 0):
-                str_stone = str(n)
-                middle_index = int(len(str_stone) / 2)
-                stones[index] = int(str_stone[:middle_index])
-                stones[index] = int(str_stone[:middle_index])
-                stones.insert(index, int(str_stone[middle_index:]))
-                shift += 1
-            else:
-                stones[index] = stones[index] * 2024
-    return stones
-
-def get_stones_after_n_blinks_efficient(i_stones, blinks):
-    cache = defaultdict(list)
-    stones = deque(i_stones)
-    for iteration in range(blinks):
-        print(iteration)
-        shift = 0
-        for i in range(len(stones)):
-            index = i + shift
-            n = stones[index]
-            if n == 0:
-                stones[index] = 1
-            elif (len(str(n)) % 2 == 0):
-                str_stone = str(n)
-                middle_index = int(len(str_stone) / 2)
-                stones[index] = int(str_stone[:middle_index])
-                stones[index] = int(str_stone[:middle_index])
-                stones.insert(index, int(str_stone[middle_index:]))
-                shift += 1
-            else:
-                stones[index] = stones[index] * 2024
-    return stones
-
-print("Initial stones")
-print(initial_stones)
-print("Stones after 25 blinks")
-stones_after_25_blinks = get_stones_after_n_blinks(initial_stones, 25)
-# print(stones_after_25_blinks)
-print(len(stones_after_25_blinks))
-stones_after_75_blinks = get_stones_after_n_blinks(initial_stones, 75)
-# print(stones_after_25_blinks)
-print(len(stones_after_75_blinks))
-
-n - {
-     iter1: n1
-     iter2: n2
-     iter3: n3
-     iter4: n4
-}
-
-0 -> 1
-1 -> [2024]
-2024 -> [20, 24]
-20 -> [2, 0]
-24 -> [2, 4]
-2 -> [2*2024]
-4 -> [4*2024]
-
-
-
-
-
+print("Garden")
+print("-" * 100)
+for row in garden:
+    print(row)
---
+++ day3_1.py
@@ -0,0 +1,40 @@
+import os
+import re
+
+from itertools import tee, islice
+
+# Get the absolute path of the current script's directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# file_path = os.path.join(script_dir, 'inputs/input_3_1_example.txt')
+file_path = os.path.join(script_dir, 'inputs/input_3_1.txt')
+
+print(f"Reading file {file_path}")
+print(f"PROCESSING PART 1...")
+content = ""
+with open(file_path, 'r') as file:
+    content = file.read()
+# print(content)
+
+def solve1(content):
+    pattern = r"mul\((\d+),(\d+)\)"
+    raw_commands = re.findall(pattern, content)
+    # print(raw_commands)
+    return sum([int(a) * int(b) for a, b in raw_commands])
+
+def isdont(it):
+    for c in "don't()":
+        if(it)
+
+
+def solve1_v2(content):
+    i = 0
+    
+    it = iter(content)
+    iter1, iter2 = tee(it, 2)
+    # even_chars = islice(iter1, 5)
+    isdont(tee(content))
+
+
+
+
+print(f"result1: {solve1(content)}")
