524fae7f259f5b0195395712152c566af13d9e78
daliusg <daliusg@wix.com>
2024-12-12 12:00:14 +0200

    auto update

--- day12.py
+++ day12.py
@@ -70,7 +70,7 @@ def get_down_coord(c):
     return move_in_direction(c, (1, 0))
 def get_up_coord(c):
     return move_in_direction(c, (-1, 0))
-def get_righ_coord(c):
+def get_right_coord(c):
     return move_in_direction(c, (0, 1))
 def get_left_coord(c):
     return move_in_direction(c, (0, -1))
@@ -89,30 +89,95 @@ def get_ranges(region):
     all_j = [j for _, j in region]
     return (min(all_i), max(all_i), min(all_j), max(all_j))
 
-def get_perimeter_and_area_with_dicount(region):
+def get_perimeter_and_area_with_dicount(region, name):
     area = len(region)
-    perimeter = 0
-    last_up = 0
     min_i, max_i, min_j, max_j = get_ranges(region)
-    last_down = 0
-    horizontal_perimeter = 0
-    coords = 
-    start_i =
-    for i, row in enumerate(region):
-        for j, c in enumerate(row):
-            up = get_up_coord(c)
 
+    print("Region")
+    print(region)
+
+    up_count = 0
+    down_count = 0
+    left_count = 0
+    right_count = 0
+    for i in range(min_i, max_i + 1):
+        has_last_up = False
+        has_last_down = False
+        for j in range(min_j, max_j + 1):
+            if not (i, j) in region: 
+                has_last_up = False
+                has_last_down = False
+                continue
+            up = get_up_coord((i, j))
+        
             if not up in region:
-                if last_up == None:
-                    last_up = []
+                if not has_last_up:
+                    has_last_up = True
+                    up_count += 1
+            else:
+                has_last_up = False
+            
+            down = get_down_coord((i, j))
+        
+            if not down in region:
+                if not has_last_down:
+                    has_last_down = True
+                    down_count += 1
+            else:
+                has_last_down = False
 
-            down = get_down_coord(c)
-            if last_up == None:
+        # if has_last_up:
+        #     up_count += 1
 
+        # if has_last_down:
+        #     down_count += 1
+
+    for j in range(min_j, max_j + 1):
+        has_last_left = False
+        has_last_right = False
+        for i in range(min_i, max_i + 1):
+            if not (i, j) in region: 
+                has_last_left = False
+                has_last_right = False
+                continue
+            left = get_left_coord((i, j))
+            print((i, j))
+        
+            if not left in region:
+                if not has_last_left:
+                    has_last_left = True
+                    left_count += 1
+            else:
+                has_last_left = False
+                # print("Added left on", (i, j))
+            
+            right = get_right_coord((i, j))
+        
+            if not right in region:
+                if not has_last_right:
+                    has_last_right = True
+                    right_count += 1
+            else:
+                has_last_right = False
+                print("Added right on", (i, j))
+
+        # if has_last_left:
+        #     # print("2Added left on", (i, j))
+        #     left_count += 1
+
+        # if has_last_right:
+        #     print("2Added right on", (i, j))
+        #     right_count += 1
+    
+    print(f"name {name} up {up_count} down {down_count} left {left_count} right {right_count}")
+    perimeter = up_count+right_count+left_count+down_count
+    print("Perimeter", perimeter)
+    return (perimeter, area)
 
 
 
 # file_path = os.path.join(script_dir, 'inputs/input_d12_example1.txt')
+# file_path = os.path.join(script_dir, 'inputs/input_d12_example0.txt')
 file_path = os.path.join(script_dir, 'inputs/input_d12.txt')
 garden = get_garden(file_path)
 print("Garden")
@@ -121,13 +186,24 @@ for row in garden:
     print(row)
 
 regions = get_regions(garden)
+# products_sum = 0
+# for region in regions:
+#     print(region)
+#     i, j = next(iter(region))
+#     name = garden[i][j]
+#     perimeter, area = get_perimeter_and_area(region)
+#     products_sum += perimeter * area
+#     print(f"Name: {name}, P: {perimeter}, A: {area}, R: {perimeter*area}")
+
+# print(f"Result1: {products_sum}")
+
 products_sum = 0
 for region in regions:
     print(region)
     i, j = next(iter(region))
     name = garden[i][j]
-    perimeter, area = get_perimeter_and_area(region)
+    perimeter, area = get_perimeter_and_area_with_dicount(region, name)
     products_sum += perimeter * area
     print(f"Name: {name}, P: {perimeter}, A: {area}, R: {perimeter*area}")
 
-print(f"Result1: {products_sum}")
+print(f"Result2: {products_sum}")
