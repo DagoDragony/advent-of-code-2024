266a90376bb63109c12e99b091c4bdfaef6e4228
daliusg <daliusg@wix.com>
2024-12-17 10:00:14 +0200

    auto update

--- day17.py
+++ day17.py
@@ -8,9 +8,8 @@ import sys
 
 sys.setrecursionlimit(10000)
 
-# FILE_PATH = 'inputs/input_d16_example1.txt'
-FILE_PATH = 'inputs/input_d16_example2.txt'
-# FILE_PATH = 'inputs/input_d16.txt'
+FILE_PATH = 'inputs/input_d17_example1.txt'
+# FILE_PATH = 'inputs/input_d17.txt'
 
 @dataclass
 class Program:
@@ -21,123 +20,126 @@ class Program:
 
     @staticmethod
     def parse(text) -> 'Program':
-		lines = text.splitlines()
-		register_A = lines[0].replace("Register A: ", "")
-		register_B = lines[1].replace("Register B: ", "")
-		register_C = lines[2].replace("Register C: ", "")
-		program = lines[4].replace("Program: ", "")
-		return Program(int(register_A),int(register_B),int(register_C),[int(op) for op in program.split(",")])
+        lines = text.splitlines()
+        register_A = lines[0].replace("Register A: ", "")
+        register_B = lines[1].replace("Register B: ", "")
+        register_C = lines[2].replace("Register C: ", "")
+        program = lines[4].replace("Program: ", "")
+        return Program(
+            int(register_A),
+            int(register_B),
+            int(register_C),
+            [int(op) for op in program.split(",")]
+        )
 
-    
+@dataclass
+class Register:
+    a: int
+    b: int
+    c: int
 
+# def get_output(operand, combo, register):
+
+def execute_program(program, A, B, C):
+	register = Register(A, B, C)
+	counter = 0
+	output = []
+	literals = set([0, 1, 2, 3])
+	# print(program)
+	while True:
+		if counter == len(program):
+			print("Program finished")
+			break
+		if counter + 1 >= len(program):
+			raise Exception("counter is too far")
+			break
+		op, combo = program[counter], program[counter + 1], 
+		print("counter", counter)
+		print(f"{op},{combo} A={register.a} B={register.b} C={register.c}")
+		
+		combo_value = -1
+		match combo:
+			case 4:
+				combo_value = register.a
+			case 5:
+				combo_value = register.b
+			case 6:
+				combo_value = register.c
+			case value if value in literals:
+				combo_value = value
+			case 7:
+				raise Exception(f"Found 7 in {counter} {op},{combo}")
+		
+		def truncate(number):
+			return number % 8
+		
+		match op:
+			case 0:
+				# adv - A division 2 in power of combo_value, send to A
+				register.a = truncate(register.a // (2^combo_value))
+			case 1:
+				# bxl - B bitwise XOR combo send to B
+				register.b = register.b ^ combo_value
+			case 2:
+				# bst - combo modulo 8, truncate, send to B
+				register.b = truncate(combo_value % 8)
+			case 3:
+				# jnz - A == 0 do nothing, A != 0 JUMP TO A
+				if register.a != 0:
+					if register.a > len(program):
+						raise Exception(f"Tried to jump outside {counter} {register.a}")
+					print(f"Set counter {register.a}")
+					counter = register.a
+					continue
+			case 4:
+				# bxc - B bitwise XOR C, send to B
+				register.b = register.b ^ register.c
+			case 5:
+				# out - combo modulo 8, output
+				print(combo_value)
+				output.append(combo_value % 8)
+			case 6:
+				# bdv - like adv, but send to B
+				register.b = register.a // (2^combo_value)
+			case 7:
+				# cdv - like adv, but send to C
+				register.c = register.a // (2^combo_value)
+		counter += 2
+	return (register, output)
+     
 
 def get_program(file_path) -> List[str]:
 	script_dir = os.path.dirname(os.path.abspath(__file__))
 	full_path = os.path.join(script_dir, file_path)
 	with open(full_path, 'r') as file:
-		lines = file.read().splitlines()
-		register_A = lines[0].replace("Register A: ", "")
-		register_B = lines[1].replace("Register B: ", "")
-		register_C = lines[2].replace("Register C: ", "")
-		program = lines[4].replace("Program: ", "")
-		return 
+		return Program.parse(file.read())
 
-
-def next_tile(position, delta):
-	i, j = position
-	di, dj = delta
-	return (i + di, j + dj)
-
-class State(Enum):
-    NO_PATH = 1
-    BACK_IN_PATH = 2
-    REACHED_END = 3
-
-@dataclass
-class NodeStatus:
-    state: State
-    value: int = 10**10
-
-cache = {}
-price = {}
-def make_a_move(pos, direction, map, visited, score):
-	i, j = pos
-	symbol = map[i][j]
-	if symbol == '#':
-		return None
-
-	if pos in price:
-		if price[pos] > score:
-			# add cheaper score to this cell
-			price[pos] = score
-		else:
-			return None
-	else:
-		price[pos] = score
-
-	match symbol:
-		case "E":
-			# cache[key] = NodeStatus(State.REACHED_END, score)
-			return
-		case v if v != "#":
-			clockwise = ROTATE_CLOCKWISE[direction]
-			counterclockwise = ROTATE_COUNTERCLOCKWISE[direction]
-			make_a_move(next_tile(pos, direction), direction, map, visited.copy(), score + 1),
-			make_a_move(next_tile(pos, clockwise), clockwise, map, visited.copy(), score + 1000 + 1),
-			make_a_move(next_tile(pos, counterclockwise), counterclockwise, map, visited.copy(), score + 1000 + 1),
-			return
-		case _:
-			return
-
-def get_all_direction(pos):
-	i, j = pos
-	return [(i + di, j + dj) for di, dj in ALL_DIRECTIONS]
-
-# visited = set()
-def walk_to_bests(pos, path, final_score, map):
-	i, j = pos
-	# visited.add(pos)
-	if map[i][j] == "E":
-		return path
-
-	def check_if_valid(new_pos):
-		result = new_pos in price and price[new_pos] < final_score # and not pos in visited
-		# if result:
-		# 	print("position to go", new_pos, price.get(new_pos))
-		return result
-	
-	all_directions = get_all_direction(pos)
-	positions_to_go = [ new_pos for new_pos in all_directions if check_if_valid(new_pos)]
-	# time.sleep(0.5)
-
-	success_paths = filter(lambda x: x != None, [walk_to_bests(pos_to_go, path | { pos }, final_score, map) for pos_to_go in positions_to_go])
-	if not success_paths:
-		return None
-	else:
-		return [position for path in success_paths for position in path]
-	
-
-def get_best_paths_tile_count(start, map): ...
-
-
-def get_score(map):
-	start = (len(map)-2, 1)
-	print(map[start[0]][start[1]])
-	end = (1, len(map)-2)
-	print(map[end[0]][end[1]])
-	make_a_move(start, EAST_DIRECTION, [list(row) for row in map], set(), 0)
-
-	final_price = price[end]
-	tile_count = walk_to_bests(start, set(), final_price, map)
-	return (final_price, tile_count)
+def test(str_program, a=0, b=0, c=0, ea=None, eb=None, ec=None, eoutput=None):
+	program = [int(l) for l in str_program.split(",")]
+	register, output = execute_program(program, a, b, c)				
+	print(output, register)
+	if ea != None:
+		assert register.a == ea
+	if eb != None:
+		assert register.b == eb
+	if ec != None:
+		assert register.c == ec
+	if eoutput != None:
+		assert output == eoutput, f"outputs didn't match actual: {output} expected: {eoutput}"
 
 def main():
-	race_map = get_map(FILE_PATH)
-	for row in race_map:
-		print(row)
-	price, tile_count = get_score(race_map)
-	print(f"Result1: {price}")
-	print(f"Result2: {tile_count}")
+	# program = get_program(FILE_PATH)
+	# print(program)
+	# register, output = execute_program(program)				
+	# print(output, register)
+
+	# test("2,6", c=9, eb=1)
+	# test("5,0,5,1,5,4", a = 10, eoutput=[0, 1, 2])
+	test("0,1,5,4,3,0", a = 2024, eoutput=[5,0,5,1,5,4], ea=0)
+
+	# If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.
+
+
 
 
 if __name__ == "__main__":
