c79245492c6789d9e59c18e49c80d7a9ca5f7413
DagoDragony <dago.dragony@gmail.com>
2024-12-18 20:00:15 +0200

    auto update

--- day16.py
+++ day16.py
@@ -4,6 +4,7 @@ from typing import Tuple, List
 from enum import Enum
 import time
 import sys
+import heapq
 
 
 sys.setrecursionlimit(10000)
@@ -119,18 +120,101 @@ def walk_to_bests(pos, path, final_score, map):
 		return [position for path in success_paths for position in path]
 	
 
-def get_best_paths_tile_count(start, map): ...
+def get_best_paths_tile_count(start, end, map):
+	visited = set()
+
+	def check_if_valid(new_pos):
+		result = new_pos in price and not new_pos in visited
+		# if result:
+		# 	print("position to go", new_pos, price.get(new_pos))
+		return result
+
+	def walk(pos):
+		print("pos", pos)
+		visited.add(pos)
+		all_directions = get_all_direction(pos)
+		positions_to_go = [new_pos for new_pos in all_directions if check_if_valid(new_pos)]
+		print("positions_to_go", positions_to_go)
+		for ptg in positions_to_go:
+			print(ptg, price[ptg])
+		if not positions_to_go:
+			return
+		# print("positions_to_go", positions_to_go)
+
+		minimum_price = min([price[ptg] for ptg in positions_to_go])
+		print("minimum_price", minimum_price)
+		final_positions_to_go = [ptg for ptg in positions_to_go if price[ptg] == minimum_price]
+		for fptg in final_positions_to_go:
+			walk(fptg)
+
+	walk(end)
+	
+	return visited
+
+def get_adjacent(start, end, map):
+
+
+
+# def get_price_path(start, max_boundary):
+# 	i, j = start
+
+
+
+
+
+
+
+# 	adj = {}
+# 	# visited = {}
+# 	for y in range(max_boundary + 1):
+# 		for x in range(max_boundary + 1):
+# 			adj[(x, y)] = (w, get_all_direction((x, y), max_boundary))
+# 			# visited.add((x, y))
+# 	# print("adj", adj)
+
+# 	shortest = {}
+# 	minHeap = [[0, start]]
+# 	while minHeap:
+# 		# print("minHeap", minHeap)
+# 		w1, n1 = heapq.heappop(minHeap)
+# 		if n1 in shortest:
+# 			continue
+# 		shortest[n1] = w1
+		
+
+# 		# print("n1", n1, "w1", w1)
+# 		# print(f"adj[{n1}]", adj[n1])
+# 		for n2 in adj[n1]:
+# 			if n2 not in shortest:
+# 				heapq.heappush(minHeap, [w1 + 1, n2])
+	
+# 	return shortest
 
 
 def get_score(map):
 	start = (len(map)-2, 1)
-	print(map[start[0]][start[1]])
+	print("start", start)
 	end = (1, len(map)-2)
-	print(map[end[0]][end[1]])
+	print("end", end)
 	make_a_move(start, EAST_DIRECTION, [list(row) for row in map], set(), 0)
 
 	final_price = price[end]
-	tile_count = walk_to_bests(start, set(), final_price, map)
+	best_paths = get_best_paths_tile_count(start, end, map)
+	tile_count = len(best_paths)
+
+	for i, row in enumerate(map):
+		line = ""
+		for j, s in enumerate(row):
+			if (i, j) in best_paths:
+				line += "^"
+			else:
+				line += s
+		print(line)
+
+	# print("price")
+	# for key, value in price.items():
+	# 	print(key, "->", value)
+
 	return (final_price, tile_count)
 
 def main():
