504cc7a8597adb05ee60b62422cbfbf2ee8223e6
daliusg <daliusg@wix.com>
2024-12-20 09:00:13 +0200

    auto update

--- day20.py
+++ day20.py
@@ -1,9 +1,9 @@
 import os
 from dataclasses import dataclass
-from typing import Tuple, List, Iterable
+from typing import Tuple, List, Iterable, TypeAlias, Dict
 from enum import Enum
 import time
-import heapq
+from heapq import heappush, heappop
 
 DAY = 20
 FILE_PATH_EXAMPLE = f"inputs/input_d{DAY}_example1.txt"
@@ -21,6 +21,8 @@ ALL_DIRECTIONS = [
 	DOWN,
 ]
 
+Coord: TypeAlias = tuple[int, int]
+
 
 def get_input(file_path) -> List[str]:
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -32,26 +34,35 @@ def get_input(file_path) -> List[str]:
 def is_outside(pos, boundaries):
 	i, j = pos
 	len_i, len_j = boundaries
-	return i < 0 or j < 0 or i >= len_i or j > len_j
+	return i < 0 or j < 0 or i >= len_i or j >= len_j
 
-
-def get_all_direction(pos, map):
+def get_all_adjacent(pos, map):
 	i, j = pos
-	all_directions =  [(i + di, j + dj) for di, dj in ALL_DIRECTIONS]
-	valid_directions = [direction for direction in all_directions if not is_outside(direction, (len(map), len(map-1)))]
+	adjacent_tiles =  [(i + di, j + dj) for di, dj in ALL_DIRECTIONS]
+	valid_directions = [tile for tile in adjacent_tiles if not is_outside(tile, (len(map), len(map[0])))]
 	return valid_directions
 
 
-def get_shortest_paths(start, end, map):
+def get_shortest_paths(start, map) -> Dict[Coord, int]:
     """
     Dijkstra again
     """
+    shortest = {}
 
     heap = [(0, start)]
     while heap:
+        current_weight, coord = heappop(heap)
+        if coord in shortest:
+            pass
+        shortest[coord] = current_weight
+        
+        all_adj = get_all_adjacent(coord, map)
+        for  tile in all_adj:
+            i, j = tile
+            if not tile in shortest and not map[i][j] == "#":
+                heappush(heap, (current_weight + 1,  tile))
+    return shortest
 		
-    print("")
-
 
 def find_tile(wanted_tile, map):
 	for i, row in enumerate(map):
@@ -60,14 +71,46 @@ def find_tile(wanted_tile, map):
 				return (i, j)
 	raise Exception(f"Couldn't find {wanted_tile} in map")
 
+def get_walkable_walls(map):
+	"""
+	Finds all walls that can be used for cheating
+ 	"""
+	row_len = len(map[0])
+	walkable_walls = []
+	for i in range(1, len(map)-1):
+		for j in range(1, row_len - 1):
+			if map[i][j] != "#":
+				continue
+
+			# walkable horizontally
+			if map[i][j -1] != "#" and map[i][j + 1] != "#":
+				walkable_walls.append(((i, j - 1), (i, j + 1)))
+
+			# walkable vertically
+			if map[i-1][j] != "#" and map[i+1][j] != "#":
+				walkable_walls.append(((i - 1, j), (i + 1, j)))
+	return walkable_walls
+
 
 def main():
 	race_map = get_input(FILE_PATH_EXAMPLE)
 	for row in race_map:
 		print(row)
-	start = find_tile("S")
-	end = find_tile("E")
-	paths = get_shortest_paths(start, end, paths)
+	start = find_tile("S", race_map)
+	end = find_tile("E", race_map)
+	shortest_paths = get_shortest_paths(start, race_map)
+	print("Shortest path", shortest_paths[end])
+	walkable_walls = get_walkable_walls(race_map)
+	saved = []
+	for walkable_wall in walkable_walls:
+		side1, side2 = walkable_wall
+		if side1 in shortest_paths and side2 in shortest_paths:
+			saved.add(abs(shortest_paths[side1] - shortest_paths[side2]) - 2)
+	print("max_saved", max(saved))
+	print(saved)
+	# print(shortest_paths)
+	# 9404 -- too high
+
 
 if __name__ == "__main__":
 	main()
