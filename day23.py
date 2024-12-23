48ab4ce60eb49688cd41cff098d2cd2a4b59d0a1
daliusg <daliusg@wix.com>
2024-12-23 10:00:12 +0200

    auto update

--- day23.py
+++ day23.py
@@ -1,8 +1,8 @@
 import os
 import os
 from typing import Tuple, List, Dict
-from collections import defaultdict
-from itertools import combinations
+from collections import defaultdict, Counter
+from itertools import combinations, groupby
 
 DAY = 23
 FILE_PATH_EXAMPLE = f"inputs/input_d{DAY}_example1.txt"
@@ -20,11 +20,7 @@ def get_input(file_path) -> List[str]:
 def normalize(a, b):
 	return (min(a, b), max(a, b))
 
-
-def main():
-	all_connections = get_input(FILE_PATH_MAIN)
-	# print(all_connections)
-
+def get_size_3_lan_parties(all_connections):
 	connected_nodes = defaultdict(set)
 	for a, b in all_connections:
 		connected_nodes[a].add(b)
@@ -46,12 +42,69 @@ def main():
 			
 	# print("LAN parties", found_LAN_parties)
 	# print("LAN count", len(found_LAN_parties))
-	print("Result1", len([ party for party in found_LAN_parties if party[0][0] == "t" or party[1][0] == "t" or party[2][0] == "t"]))
+	return len([ party for party in found_LAN_parties if party[0][0] == "t" or party[1][0] == "t" or party[2][0] == "t"])
 
+ 
+
+def get_biggest_lan_party(all_connections):
 	biggest_lan_party = []
+	connected_nodes = defaultdict(set)
+	for a, b in all_connections:
+		connected_nodes[a].add(b)
 	for pc, connections in connected_nodes.items():
 		possible_connections = [ normalize(a, b) for a, b in combinations(connections, 2) ]
 		actual_connections = [connection for connection in possible_connections if connection in all_connections]
+
+		nodes = [a for connection in actual_connections for a in connection]
+		if not nodes:
+			continue
+		
+		print("nodes", nodes)
+		counts = Counter(nodes)
+
+		duplicates = {item: count for item, count in counts.items() if count > 1}
+		if not duplicates:
+			continue
+		print(duplicates)
+
+		count_nodes = defaultdict(set)
+		for node, count in duplicates.items():
+			count_nodes[count].add(node)
+
+		print("groups", count_nodes.items())
+		print("duplicates", duplicates.items())
+
+		max_count = max([key for key, _ in count_nodes.items()])
+		print(max_count)
+
+		for count in range(max_count, 1, -1):
+			print(">>", count)
+			if count in count_nodes:
+				nodes = count_nodes[count]
+				print("nodes:", nodes)
+			if len(nodes) == count + 1:
+				return nodes + pc
+
+
+def main():
+	all_connections = get_input(FILE_PATH_EXAMPLE)
+	# print(all_connections)
+
+
+	print("Result1", get_size_3_lan_parties(all_connections))
+	print("Result2", get_biggest_lan_party(all_connections))
+
+
+			
+
+
+
+		# nodes.sort()
+		# grouped = { k: list(g) for k, g in groupby(nodes)}
+		# print(list(grouped))
+		# counts = {value: key for key, value in grouped}
+		# print(counts)
+		
 
 		
 		
